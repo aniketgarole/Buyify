e0fd1b0e14f76ee965c14be8de34c8e8acd3d1b7
Shubhangisaini205 <shubhangisaini205@gmail.com>
2023-05-04 22:15:38 +0530

    Day-2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Cart from "./User/pages/Cart";
+import Men from "./User/pages/Products/Men/Men";
 
 function App() {
   return (
     <div>
-      <Cart />
+     <Men/>
     </div>
   );
 }
