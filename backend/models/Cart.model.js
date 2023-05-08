2cb8353c91cd30d4f7b25eecf65b9495ddcbffe1
VaishnaviDhobale <masaieducation17@gmail.com>
2023-05-08 18:15:17 +0530

    update

--- backend/models/Cart.model.js
+++ backend/models/Cart.model.js
@@ -2,18 +2,19 @@ const mongoose = require("mongoose");
 
 // Cart Schema
 const cartSchema = mongoose.Schema({
-  category: String,
-  subCategory: String,
-  brand: String,
-  title: String,
-  offerPrice: Number,
-  originalPrice: Number,
-  discount: String,
-  quantity: Number,
-  images: Array,
-  size: Array,
-  rating: Number,
-  ratingCount: String,
+  // category: String,
+  // subCategory: String,
+  // brand: String,
+  // title: String,
+  // offerPrice: Number,
+  // originalPrice: Number,
+  // discount: String,
+  // quantity: Number,
+  // images: Array,
+  // size: Array,
+  // rating: Number,
+  // ratingCount: String,
+  cart : Array,
   authorId : String
 });
 
